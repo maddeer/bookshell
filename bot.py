e440cb0a153b8689c75970a4099e0bbc9e0a797e
Timofey Markov <timonnius@gmail.com>
2017-10-14 15:41:51 +0300

    update bot.py

--- bot.py
+++ bot.py
@@ -8,174 +8,44 @@ from datetime import datetime
 import magic
 import configparser
 
-#import modules_bookshell
 from modules_bookshell import save_the_book, docx_to_text, get_genre_dict
-from model.models import Genre, db_session, Book, GenreBook, Chapter, DATABASE_PATH
-
-#DATABASE_PATH = 'bookshell.db'
+from tele_bot.modules import ANSWERS, make_conv_handler
+from model.models import Genre, db_session, Book, GenreBook, Chapter
 
 config = configparser.ConfigParser()
 config.sections()
 config.read('conf/bookshell.conf')
 TELEGRAMM_KEY = config['DEFAULT']['TELEGRAMM_KEY']
 
-
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
     level=logging.INFO,
     filename='log/bot.log',
 )
-
 MAIN_MENU, CREATE_A_BOOK, ADD_CHAPTER_NAME, ADD_A_GENRE, CHOSE_NAME,\
-    CHOSE_GENRES, SAVE_MY_BOOK, UPLOAD_A_TEXT, ADD_DESCRIPTION, SAVE_BOOK = range(10)
+    CHOSE_GENRES, SAVE_MY_BOOK, UPLOAD_A_TEXT, ADD_DESCRIPTION,\
+    SAVE_BOOK = range(10)
 
 GENRE = {}
-ANSWERS = {
-    '/start': ['''
-    Добрый день!
-    Вы находитесь в библиотеке.
-    Вы можете добавить новую книгу /add_a_book
-    Отредактирвать старую /change_a_book
-    или выбрать книгу для чтения /chose_a_book
-    ''', MAIN_MENU],
-    '/add_a_book': ['''
-    Выберете один или несколько жанров книги, /chose_genres
-    Укажите название книги /chose_name
-    Название первой главы /add_chap_name
-    Напишите описние книги /add_description
-    Загрузите текст книги или первой главы /upload_a_text
-    ''', CREATE_A_BOOK],
-    '/add_a_genre': ['''
-    Укажите жанр
-    ''', ADD_A_GENRE],
-    '/chose_name': ['''
-    Укажите название книги
-    ''', CHOSE_NAME],
-    '/chose_genres': ['''
-    Выберете один или несколько жанров из предложенных,
-    или укажите свой /add_a_genre
-    Список жанров
-    ''', CHOSE_GENRES],
-    '/save_my_book': ['''
-    Книга успещно добавленна
-    ''', SAVE_MY_BOOK],
-    '/add_chap_name': ['''
-    Укажите название главы
-    ''', ADD_CHAPTER_NAME],
-    '/upload_a_text': ['''
-    Перешлите мне содержимое главы в формате .docx
-    Если вам нужно добавить еще главы,
-    вы можете сделать это в режиме редактирование книги
-    Спасибо!
-    ''', UPLOAD_A_TEXT],
-    '/add_description': ['''
-    Введите описание книги
-    ''', ADD_DESCRIPTION],
-}
 
 
 def main():
     updater = Updater(TELEGRAMM_KEY)
     dp = updater.dispatcher
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler(
-            'start',
-            command_react,
-            pass_user_data=True)],
-        states={
-            MAIN_MENU: [
-                CommandHandler(
-                    'add_a_book',
-                    command_react,
-                    pass_user_data=True),
-                CommandHandler(
-                    'change_a_book',
-                    command_react,
-                    pass_user_data=True),
-                CommandHandler(
-                    'gind_a_book',
-                    command_react,
-                    pass_user_data=True)
-            ],
-            CREATE_A_BOOK: [
-                CommandHandler(
-                    'chose_genres',
-                    command_react,
-                    pass_user_data=True),
-                CommandHandler(
-                    'chose_name',
-                    command_react,
-                    pass_user_data=True),
-                CommandHandler(
-                    'add_chap_name',
-                    command_react,
-                    pass_user_data=True),
-                CommandHandler(
-                    'upload_a_text',
-                    command_react,
-                    pass_user_data=True),
-                CommandHandler(
-                    'add_description',
-                    command_react,
-                    pass_user_data=True),
-                CommandHandler(
-                    'save_my_book',
-                    command_react,
-                    pass_user_data=True),
-                CommandHandler(
-                    'save_my_book',
-                    command_react,
-                    pass_user_data=True),
-            ],
-            CHOSE_GENRES: [
-                CommandHandler(
-                    'add_a_genre',
-                    command_react,
-                    pass_user_data=True),
-                MessageHandler(
-                    Filters.text,
-                    get_genres,
-                    pass_user_data=True),
-            ],
-            ADD_A_GENRE: [
-                MessageHandler(
-                    Filters.text,
-                    add_a_genre,
-                    pass_user_data=True),
-            ],
-            CHOSE_NAME: [
-                 MessageHandler(
-                    Filters.text,
-                    chose_name,
-                    pass_user_data=True),
-            ],
-            ADD_DESCRIPTION: [
-                 MessageHandler(
-                    Filters.text,
-                    add_description,
-                    pass_user_data=True),
-            ],
-            ADD_CHAPTER_NAME: [
-                 MessageHandler(
-                    Filters.text,
-                    add_chap_name,
-                    pass_user_data=True),
-            ],
-            UPLOAD_A_TEXT: [
-                MessageHandler(
-                    Filters.document,
-                    download_file,
-                    pass_user_data=True),
-            ],
-        },
-        fallbacks=[CommandHandler(
-            'cancel',
-            cancel,
-            pass_user_data=True)]
+    conv_handler = make_conv_handler(
+        command_handler,
+        Filters.text,
+        get_genres,
+        add_a_genre,
+        chose_name,
+        add_description,
+        add_chap_name,
+        download_file,
+        Filters.document,
+        cancel,
     )
 
     dp.add_handler(conv_handler)
-
     updater.start_polling()
     updater.idle()
 
@@ -248,7 +118,7 @@ def add_chap_name(bot, update, user_data):
     return CREATE_A_BOOK
 
 
-def command_react(bot, update, user_data):
+def command_handler(bot, update, user_data):
     global ANSWERS
     answer = ANSWERS[update.message.text][0]
     if update.message.text == '/save_my_book':
